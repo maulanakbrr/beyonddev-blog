dff50b213603b62cb3a3ea9ea1e2782929b3eea2
Akbar Maulana <amaulanaa995@gmail.com>
2022-10-17 11:22:45 +0700

    added new posts static file, installed gray-matter library, updated Homepage and Layout component

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/Layout"
-
-export default function HomePage() {
-  return (
-    <Layout>
-      Beyond Dev
-    </Layout>
-  )
-}
---
+++ pages/index.jsx
@@ -0,0 +1,34 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Layout from "../components/Layout"
+
+export default function HomePage(props) {
+  return (
+    <Layout>
+      Beyond Dev
+    </Layout>
+  )
+}
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map(filename => {
+    const slug = filename.replace('.md', '')
+    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+
+    const { data:frontmatter } = matter(markdownWithMeta)
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+  return {
+    props: {
+      posts
+    },
+
+  }
+}
